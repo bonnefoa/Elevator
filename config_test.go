995f27e830824993102c3af762e7deb28ad6aa00
Sora <anthonin.bonnefoy@gmail.com>
2013-08-02 08:58:59 +0200

    Added simple test for ini parse check

---
+++ config_test.go
@@ -0,0 +1,18 @@
+package elevator
+
+import (
+	"testing"
+    "reflect"
+)
+
+func TestConfigFromPath(t *testing.T) {
+    expected := NewConfig()
+	c := &Config{
+		&CoreConfig{},
+		&StorageEngineConfig{},
+	}
+	c.FromFile("conf/elevator.conf")
+	if !reflect.DeepEqual(c, expected) {
+		t.Fatalf("\nExpected \n%q\ngot \n%q", expected, c)
+	}
+}
