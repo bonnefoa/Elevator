e361987ec9b0b915c4d12df7621bc2d2ab0778e0
Sora <anthonin.bonnefoy@gmail.com>
2013-08-03 22:45:56 +0200

    Handle basic store and db request on cli

--- cli/cli.go
+++ cli/cli.go
@@ -9,34 +9,73 @@ import (
 	"os"
 )
 
-type CliState struct {
+type cliState struct {
 	endpoint   *string
+	currentDb  *string
 	currentUid *string
 	socket     *zmq.Socket
 }
 
+func printHelp() {
+	fmt.Printf(
+		`DBCREATE  <dbname>
+DBDROP    <dbname>
+DBCONNECT <dbname>
+DBLIST
+GET <key>
+PUT <key> <value>
+`)
+}
+
+func processRequest(state *cliState, scanner *bufio.Scanner, socket *zmq.Socket) *elevator.Request {
+	scanner.Scan()
+	l := scanner.Text()
+	if l == "help" {
+		printHelp()
+		return nil
+	}
+	request, err := elevator.RequestFromString(l)
+	if err != nil {
+		fmt.Print(err)
+		fmt.Print("\n")
+		return nil
+	}
+	if state.currentUid != nil {
+		request.DbUid = *state.currentUid
+	}
+	request.SendRequest(socket)
+	return request
+}
+
 func main() {
-	conf := &CliState{}
-	conf.endpoint = flag.String("e", elevator.DEFAULT_ENDPOINT, "Endpoint of elevator")
+	state := &cliState{}
+	state.endpoint = flag.String("e",
+		elevator.DEFAULT_ENDPOINT, "Endpoint of elevator")
 	flag.Parse()
 
 	context, _ := zmq.NewContext()
 	socket, _ := context.NewSocket(zmq.REQ)
-	socket.Connect(*conf.endpoint)
-	conf.socket = socket
+	socket.Connect(*state.endpoint)
+	state.socket = socket
 
 	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		l := scanner.Text()
-		req, err := elevator.RequestFromString(l)
-		if err != nil {
-			fmt.Print(err)
-			fmt.Print("\n\n")
+	for {
+		if state.currentUid == nil {
+			fmt.Print("$> ")
+		} else {
+			fmt.Printf("(%s) $> ", *state.currentDb)
+		}
+		request := processRequest(state, scanner, socket)
+		if request == nil {
 			continue
 		}
-		req.SendRequest(socket)
 		response := elevator.ReceiveResponse(socket)
+		if request.Command == elevator.DB_CONNECT &&
+			response.Status == elevator.SUCCESS_STATUS {
+			state.currentDb = &request.Args[0]
+			state.currentUid = &response.Data[0]
+		}
 		fmt.Print(response)
-		fmt.Print("\n\n")
+		fmt.Print("\n")
 	}
 }
