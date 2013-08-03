832a907cdf61f6f1063d1a1a71309e39ea770791
Sora <anthonin.bonnefoy@gmail.com>
2013-08-03 22:41:10 +0200

    Reuse store_command and db_command for request parse

--- request.go
+++ request.go
@@ -15,25 +15,6 @@ type Request struct {
 	Source  *ClientSocket `msgpack:"-"`
 }
 
-var DatabaseRequest = map[string]bool{
-	DB_GET:    true,
-	DB_MGET:   true,
-	DB_PUT:    true,
-	DB_DELETE: true,
-	DB_RANGE:  true,
-	DB_SLICE:  true,
-	DB_BATCH:  true,
-}
-
-var DbstoreRequests = map[string]bool{
-	DB_CREATE:  true,
-	DB_DROP:    true,
-	DB_CONNECT: true,
-	DB_MOUNT:   true,
-	DB_UMOUNT:  true,
-	DB_LIST:    true,
-}
-
 // String represents the Request as a normalized string
 func (r *Request) String() string {
 	return fmt.Sprintf("<Request uid:%s command:%s args:%s>",
@@ -46,20 +27,9 @@ func RequestFromString(req string) (*Request, error) {
 		return nil, errors.New("Empty request")
 	}
 	cmd := words[0]
-	if _, exist := DatabaseRequest[cmd]; exist {
-		if len(words) < 3 {
-			return nil, errors.New(
-				fmt.Sprintf("Expected CMD DBUID ARGS, got %s", req))
-		}
-		dbuid := words[1]
-		args := words[2:]
-		return &Request{DbUid: dbuid, Command: cmd, Args: args}, nil
-	}
-	if _, exist := DbstoreRequests[cmd]; exist {
-		if len(words) < 2 {
-			return nil, errors.New(
-				fmt.Sprintf("Expected CMD ARGS, got %s", req))
-		}
+	_, exist_store := store_commands[cmd]
+	_, exist_db := database_commands[cmd]
+	if exist_store || exist_db {
 		args := words[1:]
 		return &Request{Command: cmd, Args: args}, nil
 	}
@@ -67,17 +37,8 @@ func RequestFromString(req string) (*Request, error) {
 		fmt.Sprintf("Unknown command %s", req))
 }
 
-// NewRequest returns a pointer to a brand new allocated Request
-func NewRequest(command string, args []string) *Request {
-	return &Request{
-		Command: command,
-		Args:    args,
-	}
-}
-
 func (request *Request) SendRequest(socket *zmq.Socket) {
 	var buffer bytes.Buffer
 	PackInto(request, &buffer)
 	socket.SendMultipart([][]byte{buffer.Bytes()}, 0)
-
 }
